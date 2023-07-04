6246c49226fcfd870c772f87e0b6403870fc2f9c
Robert Love <73248528+R-LoveIV@users.noreply.github.com>
2023-07-04 00:02:43 -0500

    Add files via upload

---
+++ Conditionals.java
@@ -0,0 +1,13 @@
+
+// write a conditional statement to decide if someone is of legal age to buy a drink.
+public class Conditionals {
+    public static void main(String[] args){
+        int age = 29;
+
+        if (age >= 21){
+            System.out.println("You're old enough to buy a drink");
+        } else {
+            System.out.println("You're not old enough. Come back when you're 21");
+        }
+    }
+}
